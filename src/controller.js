cf6dddfa2249f4981b02bd293a989ce513278c20
vikas <106076457+VikasU123@users.noreply.github.com>
2023-07-21 17:30:50 +0530

    add center button to controller

--- src/controller.js
+++ src/controller.js
@@ -12,6 +12,7 @@ class Controller extends Component{
                     <img className={styles.left_button} src="https://cdn-icons-png.flaticon.com/512/26/26309.png" />
                     <img className={styles.right_button} src="https://cdn-icons-png.flaticon.com/512/26/26309.png" />
                     <img className={styles.bottem_button} src="https://cdn-icons-png.flaticon.com/512/64/64595.png" />
+                    <div className={styles.center_button}></div>
                 </div>
             </div>
             </>
